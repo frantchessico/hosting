2cbb21310e0484d330a14382480b92adb5554e68
Francisco  Inoque <40237204+frantchessico@users.noreply.github.com>
2020-01-13 06:29:35 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
  const mongoose = require('mongoose');
  const mongoDb = process.env.MONGODB_URI
 mongoose.connect(mongoDb,{
-useNewUrlParser: true
+useNewUrlParser: true,
+ useUnifiedTopology: true
 }).then(() => {
     console.log('DB is connected')
 }).catch(err => console.log(err))
